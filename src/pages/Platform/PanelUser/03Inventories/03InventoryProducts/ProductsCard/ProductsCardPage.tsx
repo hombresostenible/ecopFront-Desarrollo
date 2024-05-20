cc188b6e8de10449590c74b6ec9bcd36070790a8
CarlosMario-Pro <cmario.reyesp@gmail.com>
2024-05-20 07:44:37 -0500

    Se integran los componentes de Products

--- src/pages/Platform/PanelUser/03Inventories/03InventoryProducts/ProductsCard/ProductsCardPage.tsx
+++ src/pages/Platform/PanelUser/03Inventories/03InventoryProducts/ProductsCard/ProductsCardPage.tsx
@@ -4,29 +4,29 @@ import jsCookie from 'js-cookie';
 import { Modal } from 'react-bootstrap';
 // REDUX
 import { useDispatch, useSelector } from 'react-redux';
-import { getMerchandises, getMerchandisesByBranch } from '../../../../../../redux/User/merchandiseSlice/actions';
+import { getProducts, getProductsByBranch } from '../../../../../../redux/User/productSlice/actions';
 import { getBranches } from '../../../../../../redux/User/branchSlice/actions';
 import type { RootState, AppDispatch } from '../../../../../../redux/store';
 // ELEMENTOS DEL COMPONENTE
-import { IMerchandise } from '../../../../../../types/User/merchandise.types';
+import { IProduct } from '../../../../../../types/User/products.types';
 import { IBranch } from '../../../../../../types/User/branch.types';
 import NavBar from '../../../../../../components/Platform/NavBar/NavBar';
 import SideBar from '../../../../../../components/Platform/SideBar/SideBar';
 import Footer from '../../../../../../components/Platform/Footer/Footer';
-import ModalMerchandises from '../../../../../../components/Platform/03Inventories/Merchandises/ModalMerchandises/ModalMerchandises';
-import ModalMerchadiseOff from '../../../../../../components/Platform/03Inventories/Merchandises/ModalMerchadiseOff/ModalMerchadiseOff';
+import ModalProduct from '../../../../../../components/Platform/03Inventories/Products/ModalProduct/ModalProduct';
+import ModalProductOff from '../../../../../../components/Platform/03Inventories/Products/ModalProductOff/ModalProductOff';
 import ConfirmDeleteRegister from '../../../../../../components/Platform/03Inventories/ConfirmDeleteRegister';
 import { RiDeleteBin6Line } from 'react-icons/ri';
 import { BsPencil } from 'react-icons/bs';
 import { IoIosCloseCircleOutline } from "react-icons/io";
 import styles from './styles.module.css';
 
-function MerchadisesCardPage() {
+function InventoryProductsPage() {
     const token = jsCookie.get('token') || '';
     const dispatch: AppDispatch = useDispatch();
 
     ///ESTADOS DE REDUX
-    const merchandise = useSelector((state: RootState) => state.merchandise.merchandise);
+    const product = useSelector((state: RootState) => state.product.product);
     const branches = useSelector((state: RootState) => state.branch.branch);
 
     const [selectedBranch, setSelectedBranch] = useState<string | undefined>('');
@@ -34,40 +34,41 @@ function MerchadisesCardPage() {
     useEffect(() => {
         if (token) {
             dispatch(getBranches(token));
-            dispatch(getMerchandises(token));
+            dispatch(getProducts(token));
         }
     }, [token]);
 
+    //PARA FILTRAR POR SEDE O POR TODAS
     useEffect(() => {
         if (token) {
             if (selectedBranch) {
-                dispatch(getMerchandisesByBranch(selectedBranch, token));
+                dispatch(getProductsByBranch(selectedBranch, token));
             } else {
                 // Si no se selecciona ninguna sede, obtén todos los activos
-                dispatch(getMerchandises(token));
+                dispatch(getProducts(token));
             }
         }
     }, [selectedBranch, token, dispatch]);
 
-    const [idMerchadise, setIdMerchadise] = useState('');
-    const [nameMerchadise, setNameMerchadise] = useState('');
+    const [idProduct, setIdProduct] = useState('');
+    const [nameProduct, setNameProduct] = useState('');
     const [showDeleteConfirmation, setShowDeleteConfirmation] = useState(false);
-    const [selectedItem, setSelectedItem] = useState<IMerchandise>();
+    const [selectedItem, setSelectedItem] = useState<IProduct>();
     const [showItemModal, setShowItemModal] = useState(false);
     const [showOff, setShowOff] = useState(false);
 
-    const handleDelete = useCallback((merchadise: IMerchandise) => {
-        setSelectedItem(merchadise);
+    const handleDelete = useCallback((product: IProduct) => {
+        setSelectedItem(product);
         setShowDeleteConfirmation(true);
     }, []);
 
-    const handleEdit = useCallback((merchadise: IMerchandise) => {
-        setSelectedItem(merchadise);
+    const handleEdit = useCallback((product: IProduct) => {
+        setSelectedItem(product);
         setShowItemModal(true);
     }, []);
 
-    const handleOff = useCallback((merchadise: IMerchandise) => {
-        setSelectedItem(merchadise);
+    const handleOff = useCallback((product: IProduct) => {
+        setSelectedItem(product);
         setShowOff(true);
     }, []);
 
@@ -88,9 +89,9 @@ function MerchadisesCardPage() {
                     <div className={`${styles.container__Component} overflow-hidden overflow-y-auto`}>
                         <div className='mt-4 border d-flex flex-column align-items-center justify-content-center'>
                             <div className='p-4'>
-                                <h1 className='text-2xl font-bold'>Mercancías</h1>
+                                <h1 className='text-2xl font-bold'>Productos</h1>
                             </div>
-                            <h2>Filtra tus mercancías por sede</h2>
+                            <h2>Filtra tus productos por sede</h2>
                             <select
                                 value={selectedBranch || ''}
                                 className="mx-2 p-3 mb-3 m-center col-lg-5 col-md-4 col-sm-6 col-xs-12 text-center border rounded"
@@ -119,51 +120,51 @@ function MerchadisesCardPage() {
                                 </div>
                             </div>
                             <div className={`${styles.container__Body} d-flex flex-column `}>
-                                {Array.isArray(merchandise) && merchandise.map((merchadise) => (
-                                    <div key={merchadise.id} className={`${styles.container__Info} d-flex align-items-center justify-content-between`} >
+                                {Array.isArray(product) && product.map((product) => (
+                                    <div key={product.id} className={`${styles.container__Info} d-flex align-items-center justify-content-between`} >
                                         <div className={`${styles.column__Branch} d-flex align-items-center justify-content-start`}>
-                                            <span className={`${styles.text__Ellipsis} overflow-hidden`}>{merchadise.branchId}</span>
+                                            <span className={`${styles.text__Ellipsis} overflow-hidden`}>{product.branchId}</span>
                                         </div>
                                         <div className={`${styles.column__Name_Item} d-flex align-items-center justify-content-start`}>
-                                            <span className={`${styles.text__Ellipsis} overflow-hidden`}>{merchadise.nameItem}</span>
+                                            <span className={`${styles.text__Ellipsis} overflow-hidden`}>{product.nameItem}</span>
                                         </div>
                                         <div className={`${styles.column__Inventory} pt-0 pb-0 px-2 d-flex align-items-center justify-content-center overflow-hidden`}>
-                                            <span className={`${styles.text__Ellipsis} overflow-hidden`}>{merchadise.inventory}</span>
+                                            <span className={`${styles.text__Ellipsis} overflow-hidden`}>{product.inventory}</span>
                                         </div>
                                         <div className={`${styles.column__Unit_Measure} pt-0 pb-0 px-2 d-flex align-items-center justify-content-center overflow-hidden`}>
-                                            <span className={`${styles.text__Ellipsis} overflow-hidden`}>{merchadise.unitMeasure}</span>
+                                            <span className={`${styles.text__Ellipsis} overflow-hidden`}>{product.unitMeasure}</span>
                                         </div>
                                         <div className={`${styles.column__Selling_Price} pt-0 pb-0 px-2 d-flex align-items-center justify-content-center overflow-hidden`}>
-                                            <span className={`${styles.text__Ellipsis} overflow-hidden`}>{merchadise.sellingPrice}</span>
+                                            <span className={`${styles.text__Ellipsis} overflow-hidden`}>{product.sellingPrice}</span>
                                         </div>
                                         <div className={`${styles.column__Packaged} pt-0 pb-0 px-2 d-flex align-items-center justify-content-center overflow-hidden`}>
-                                            <span className={`${styles.text__Ellipsis} overflow-hidden`}>{merchadise.packaged}</span>
+                                            <span className={`${styles.text__Ellipsis} overflow-hidden`}>{product.packaged}</span>
                                         </div>
                                         <div className={`${styles.column__Primary_Package_Type} pt-0 pb-0 px-2 d-flex align-items-center justify-content-start overflow-hidden`}>
-                                            <span className={`${styles.text__Ellipsis} overflow-hidden`}>{merchadise.primaryPackageType}</span>
+                                            <span className={`${styles.text__Ellipsis} overflow-hidden`}>{product.primaryPackageType}</span>
                                         </div>
                                         <div className={`${styles.column__Action} pt-0 pb-0 px-2 d-flex align-items-center justify-content-start overflow-hidden`}>
                                             <RiDeleteBin6Line
                                                 className={`${styles.button__Delete} d-flex align-items-center justify-content-center`}
                                                 onClick={() => {
-                                                    setIdMerchadise(merchadise.id);
-                                                    setNameMerchadise(merchadise.nameItem || '');
-                                                    handleDelete(merchadise);
+                                                    setIdProduct(product.id);
+                                                    setNameProduct(product.nameItem || '');
+                                                    handleDelete(product);
                                                 }}
                                             />
                                             <BsPencil
                                                 className={`${styles.button__Edit} d-flex align-items-center justify-content-center`}
                                                 onClick={() => {
-                                                    setIdMerchadise(merchadise.id);
-                                                    handleEdit(merchadise)
+                                                    setIdProduct(product.id);
+                                                    handleEdit(product)
                                                 }}
                                             />
                                             <IoIosCloseCircleOutline
                                                 className={`${styles.button__Edit} d-flex align-items-center justify-content-center`}
                                                 onClick={() => {
-                                                    setIdMerchadise(merchadise.id);
-                                                    setNameMerchadise(merchadise.nameItem || '');
-                                                    handleOff(merchadise)
+                                                    setIdProduct(product.id);
+                                                    setNameProduct(product.nameItem || '');
+                                                    handleOff(product)
                                                 }}
                                             />
                                         </div>
@@ -174,14 +175,14 @@ function MerchadisesCardPage() {
 
                         <Modal show={showItemModal} onHide={onCloseModal} size="xl">
                             <Modal.Header closeButton>
-                                <Modal.Title className='text-primary-emphasis text-start'>Detalles de tu mercancía</Modal.Title>
+                                <Modal.Title className='text-primary-emphasis text-start'>Detalles de tu producto</Modal.Title>
                             </Modal.Header>
                             <Modal.Body>
                                 {selectedItem &&
-                                    <ModalMerchandises
+                                    <ModalProduct
                                         token={token}
-                                        idItem={idMerchadise}
-                                        merchandise={selectedItem}
+                                        idItem={idProduct}
+                                        product={selectedItem}
                                         branches={branchesArray}
                                         onCloseModal={onCloseModal}
                                     />
@@ -191,13 +192,13 @@ function MerchadisesCardPage() {
 
                         <Modal show={showDeleteConfirmation} onHide={() => setShowDeleteConfirmation(false)} >
                             <Modal.Header closeButton>
-                                <Modal.Title className='text-primary-emphasis text-start'>Confirmación para eliminar la mercancía "{nameMerchadise}"</Modal.Title>
+                                <Modal.Title className='text-primary-emphasis text-start'>Confirmación para eliminar el produto "{nameProduct}"</Modal.Title>
                             </Modal.Header>
                             <Modal.Body>
                                 <ConfirmDeleteRegister
-                                    typeRegisterDelete={'Merchandise'}
-                                    idItem={idMerchadise}
-                                    nameRegister={nameMerchadise}
+                                    typeRegisterDelete={'Product'}
+                                    idItem={idProduct}
+                                    nameRegister={nameProduct}
                                     onCloseModal={onCloseModal}
                                 />
                             </Modal.Body>
@@ -205,12 +206,12 @@ function MerchadisesCardPage() {
 
                         <Modal show={showOff} onHide={() => setShowOff(false)} >
                             <Modal.Header closeButton>
-                                <Modal.Title className='text-primary-emphasis text-start'>Confirmación para dar de baja del inventario de mercancías</Modal.Title>
+                                <Modal.Title className='text-primary-emphasis text-start'>Confirmación para dar de baja del inventario de productos</Modal.Title>
                             </Modal.Header>
                             <Modal.Body>
-                                <ModalMerchadiseOff
+                                <ModalProductOff
                                     token={token}
-                                    merchandise={selectedItem as IMerchandise}
+                                    product={selectedItem as IProduct}
                                     onCloseModal={onCloseModal}
                                 />
                             </Modal.Body>
@@ -223,4 +224,4 @@ function MerchadisesCardPage() {
     );
 }
 
-export default MerchadisesCardPage;
+export default InventoryProductsPage;
