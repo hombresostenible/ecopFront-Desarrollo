66694f17c12f463416cb0feae7491bfacff6195b
TopDriveGroup <carlos.reyes@topdrivegroup.com>
2024-07-03 22:48:31 -0500

    Se crea el helper SelectIVA para seleccion de meanPayment al crear o editar

--- src/helpers/Selects/IVA/IVA.ts
+++ src/helpers/Selects/IVA/IVA.ts
@@ -1,7 +1,7 @@
-const productUnitMeasure: { value: string; label: string }[] = [
+const ivaSelects: { value: string; label: string }[] = [
     {value: '0', label: '0'},
     {value: '5', label: '5'},
     {value: '19', label: '19'},
 ];
 
-export default productUnitMeasure;
+export default ivaSelects;
