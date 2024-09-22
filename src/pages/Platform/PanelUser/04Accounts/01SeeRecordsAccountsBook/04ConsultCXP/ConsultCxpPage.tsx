74ee9cce9b253adeb591f1442b65f8aa1eeba783
Carlos Mario Reyes <cmario.reyesp@gmail.com>
2024-09-22 17:03:11 -0500

    Se crean paginas para renderizar ingresos, gastos

---
+++ src/pages/Platform/PanelUser/04Accounts/01SeeRecordsAccountsBook/04ConsultCXP/ConsultCxpPage.tsx
@@ -0,0 +1,28 @@
+/* eslint-disable @typescript-eslint/no-explicit-any, react-hooks/exhaustive-deps, @typescript-eslint/no-unused-vars */
+// REDUX
+// ELEMENTOS DEL COMPONENTE
+import NavBar from '../../../../../../components/Platform/PanelUser/00NavBar/NavBar.tsx';
+import SideBar from '../../../../../../components/Platform/SideBar/SideBar.tsx';
+import Footer from '../../../../../../components/Platform/PanelUser/Footer/Footer';
+import styles from './styles.module.css';
+
+function ConsultCxpPage() {
+
+    return (
+        <div className='d-flex flex-column'>
+            <NavBar />
+            <div className='d-flex'>
+                <SideBar />
+                <div className={`${styles.container} d-flex flex-column align-items-center justify-content-between overflow-hidden overflow-y-auto`}>
+                    <div className={`${styles.container__Component} px-5 overflow-hidden overflow-y-auto`}>
+                        <h1 className={`${styles.title} mb-4 mt-4 mx-auto`}>Ver todas cuentas por pagar</h1>
+
+                    </div>
+                    <Footer />
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default ConsultCxpPage;
